6d96b1c850cab6acf1b8583a8d089e2907d433fe
Nic-Wallace <>
2023-05-12 17:50:38 +0000

    Add timer to js and quiz.html

--- assets/js/script.js
+++ assets/js/script.js
@@ -142,3 +142,34 @@ function proceedQuiz() {
         `;
     }
 }
+
+const timer = document.getElementById('timer');
+let timerInterval;
+
+startTimer = () => {
+    let second = 0,
+      minute = 0,
+      hour = 0;
+  
+    timerInterval = setInterval(function () {
+      timer.classList.toggle('odd');
+  
+      timer.innerHTML =
+        (hour ? hour + ':' : '') +
+        (minute < 10 ? '0' + minute : minute) +
+        ':' +
+        (second < 10 ? '0' + second : second);
+
+      second++;
+  
+      if (second == 60) {
+        minute++;
+        second = 0;
+      }
+  
+      if (minute == 60) {
+        hour++;
+        minute = 0;
+      }
+    }, 1000);
+  };
